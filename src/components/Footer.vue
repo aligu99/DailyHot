7d1c6ffd8c0781cca933c4dd309d7a6e0da734f3
aligu99 <zhangsan200022@gmail.com>
2024-08-02 16:45:29 +0800

    修改图标和页脚

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -2,27 +2,27 @@
   <footer>
     <div class="copyright">
       <n-text class="description" v-html="packageJson.description" />
-      <n-text
-        class="author"
-        :depth="3"
-        v-html="packageJson.author"
-        @click="jumpLink(packageJson.github)"
-      />
+      <span class="author" @click="jumpLink(authorUrl)">
+        {{ authorText }}
+      </span>
+      <span> &nbsp;| API from</span>
+      <span class="api-provider" @click="jumpLink(authorUrl)">
+      aligu99
+      </span>
+      <span> &nbsp;| Designed by</span>
+      <span class="designer" @click="jumpLink(designUrl)">
+      imsyy
+      </span>
     </div>
-    <n-text
-      v-if="icp"
-      :depth="3"
-      class="icp"
-      v-html="icp"
-      @click="jumpLink('https://beian.miit.gov.cn/')"
-    />
   </footer>
 </template>
 
 <script setup>
 import packageJson from "@/../package.json";
 
-const icp = ref(import.meta.env.VITE_ICP ? import.meta.env.VITE_ICP : null);
+const authorUrl = packageJson.github; // 使用作者的 GitHub 链接
+const authorText = packageJson.author;
+const designUrl = packageJson.GitHub;
 
 // 链接跳转
 const jumpLink = (url) => {
@@ -40,7 +40,10 @@ footer {
   flex-direction: column;
   align-items: center;
   justify-content: center;
+
   .copyright {
+    display: flex;
+    align-items: center;
     margin-bottom: 4px;
     .description {
       &::after {
@@ -49,20 +52,26 @@ footer {
       }
     }
   }
-  .author {
+
+  .author, .api-provider, .designer {
     cursor: pointer;
     transition: all 0.3s;
+    color: grey; /* 更浅的颜色 */
     &:hover {
-      color: var(--n-code-text-color);
+      color: var(--n-code-text-color); /* 更深的颜色 */
     }
   }
-  .icp {
-    font-size: 13px;
+
+  .api-provider, .designer {
+    margin-left: 2px; /* 减小间距 */
+  }
+
+  span:not(.author):not(.api-provider):not(.designer) {
+    margin: 0 1px; /* 减小间距 */
+  }
+
+  .clickable {
     cursor: pointer;
-    transition: all 0.3s;
-    &:hover {
-      color: var(--n-code-text-color);
-    }
   }
 }
 </style>
